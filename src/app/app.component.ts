a1484e3fad926b5a066fb79f4aa25589bc0a6ba7
Chris <elhalabichristian@gmail.com>
2024-03-28 22:33:02 +0200

    Add printing request model, import PrimeNG styles, and update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RequestFormComponent } from './components/request-form/request-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RequestFormComponent],
 })
 export class AppComponent {
   title = 'printer-queue';
---
+++ src/app/models/printing-request.model.ts
@@ -0,0 +1,5 @@
+export interface PrintRequest {
+  userId: number;
+  document: string;
+  priority: number;
+}
